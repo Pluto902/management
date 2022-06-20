e281773ffb0cccf40527c16d56d2b0ff0b3f5084
zhangyan3 <zhangyan@yuantutech.com>
2022-06-20 13:33:00 +0800

    初始化

---
+++ common/src/main/java/com/zhangyan/common/constant/Constant.java
@@ -0,0 +1,21 @@
+package com.zhangyan.common.constant;
+
+/**
+ * TODO
+ *
+ * @author ZhangYan
+ * @version V1.0
+ * @since 2022-06-20 10:58
+ * 管理系统常量
+ */
+public class Constant {
+    /**
+     * http请求
+     */
+    public static final String HTTP = "http://";
+
+    /**
+     * https请求
+     */
+    public static final String HTTPS = "https://";
+}
