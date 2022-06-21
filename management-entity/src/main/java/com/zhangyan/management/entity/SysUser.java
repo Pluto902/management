92c592db0fbb67dd701c3bd59aacfbd2c26946e0
zhangyan3 <zhangyan@yuantutech.com>
2022-06-21 17:02:26 +0800

    初始化

---
+++ management-entity/src/main/java/com/zhangyan/management/entity/SysUser.java
@@ -0,0 +1,245 @@
+package com.zhangyan.management.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * 系统用户表(SysUser)实体类
+ *
+ * @author makejava
+ * @since 2022-06-21 11:18:47
+ */
+public class SysUser implements Serializable {
+    private static final long serialVersionUID = -21594473980607725L;
+    /**
+     * 用户主键
+     */
+    private Long userId;
+    /**
+     * 部门id
+     */
+    private Long deptId;
+    /**
+     * 用户账号
+     */
+    private String userName;
+    /**
+     * 用户昵称
+     */
+    private String name;
+    /**
+     * 用户邮箱
+     */
+    private String email;
+    /**
+     * 手机号码
+     */
+    private String phonenumber;
+    /**
+     * 用户性别（0男 1女 2未知）
+     */
+    private String sex;
+    /**
+     * 头像地址
+     */
+    private String avatar;
+    /**
+     * 密码
+     */
+    private String password;
+    /**
+     * 帐号状态（0正常 1停用）
+     */
+    private String status;
+    /**
+     * 删除标志（0代表存在 2代表删除）
+     */
+    private String delFlag;
+    /**
+     * 最后登录IP
+     */
+    private String loginIp;
+    /**
+     * 最后登录时间
+     */
+    private Date loginDate;
+    /**
+     * 创建者
+     */
+    private String createBy;
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+    /**
+     * 更新者
+     */
+    private String updateBy;
+    /**
+     * 更新时间
+     */
+    private Date updateTime;
+    /**
+     * 备注
+     */
+    private String remark;
+    /**
+     * 超级管理员 0是 1不是（防止被删除）
+     */
+    private String superAdmin;
+
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getDeptId() {
+        return deptId;
+    }
+
+    public void setDeptId(Long deptId) {
+        this.deptId = deptId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhonenumber() {
+        return phonenumber;
+    }
+
+    public void setPhonenumber(String phonenumber) {
+        this.phonenumber = phonenumber;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getDelFlag() {
+        return delFlag;
+    }
+
+    public void setDelFlag(String delFlag) {
+        this.delFlag = delFlag;
+    }
+
+    public String getLoginIp() {
+        return loginIp;
+    }
+
+    public void setLoginIp(String loginIp) {
+        this.loginIp = loginIp;
+    }
+
+    public Date getLoginDate() {
+        return loginDate;
+    }
+
+    public void setLoginDate(Date loginDate) {
+        this.loginDate = loginDate;
+    }
+
+    public String getCreateBy() {
+        return createBy;
+    }
+
+    public void setCreateBy(String createBy) {
+        this.createBy = createBy;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getUpdateBy() {
+        return updateBy;
+    }
+
+    public void setUpdateBy(String updateBy) {
+        this.updateBy = updateBy;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+
+    public String getSuperAdmin() {
+        return superAdmin;
+    }
+
+    public void setSuperAdmin(String superAdmin) {
+        this.superAdmin = superAdmin;
+    }
+
+}
+
---
+++ management-web/src/test/java/com/zhangyan/management/ManagementWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhangyan.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
